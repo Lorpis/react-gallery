bd22b72044b1209f1f115c9831e7da10a027f266
Your Name <wieber99@hotmail.com>
2019-06-17 18:57:19 -0500

    Updates

---
+++ precache-manifest.496425c5e9d81567fe7b458058dadce7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca71e7e4a896a34b05d1285575f142c",
+    "url": "/react-gallery/index.html"
+  },
+  {
+    "revision": "e9413bfd23628853eb90",
+    "url": "/react-gallery/static/css/main.454d8b16.chunk.css"
+  },
+  {
+    "revision": "cff9c27cc83fad3ea1d8",
+    "url": "/react-gallery/static/js/2.abb28d26.chunk.js"
+  },
+  {
+    "revision": "e9413bfd23628853eb90",
+    "url": "/react-gallery/static/js/main.fefbecf8.chunk.js"
+  },
+  {
+    "revision": "c00525b39cf2502cdaf0",
+    "url": "/react-gallery/static/js/runtime~main.8fccbdb2.js"
+  },
+  {
+    "revision": "e0f53715036229c31a963b2761053131",
+    "url": "/react-gallery/static/media/code.e0f53715.jpg"
+  },
+  {
+    "revision": "fbf20f513d3c0c4e533d25058157b2d0",
+    "url": "/react-gallery/static/media/react.fbf20f51.png"
+  }
+]);
