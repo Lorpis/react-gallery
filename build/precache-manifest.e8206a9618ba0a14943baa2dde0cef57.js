ea67b82de5bb543518a5a75d08f895d6eb1b6912
Your Name <wieber99@hotmail.com>
2019-12-25 09:44:44 -0600

    added voltage drop

---
+++ build/precache-manifest.e8206a9618ba0a14943baa2dde0cef57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b19548006a17190ae58ac2b2818d22",
+    "url": "/react-gallery/index.html"
+  },
+  {
+    "revision": "8edb2c890e94f08b5efe",
+    "url": "/react-gallery/static/css/main.53c01dae.chunk.css"
+  },
+  {
+    "revision": "65d65b8adc43335bba31",
+    "url": "/react-gallery/static/js/2.9d0e0e56.chunk.js"
+  },
+  {
+    "revision": "2f33dc5ac6616a7770b8117950a39b8a",
+    "url": "/react-gallery/static/js/2.9d0e0e56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8edb2c890e94f08b5efe",
+    "url": "/react-gallery/static/js/main.2e38980b.chunk.js"
+  },
+  {
+    "revision": "0b13b2a8ac89d3e06054",
+    "url": "/react-gallery/static/js/runtime-main.759fc235.js"
+  },
+  {
+    "revision": "e0f53715036229c31a963b2761053131",
+    "url": "/react-gallery/static/media/code.e0f53715.jpg"
+  },
+  {
+    "revision": "fbf20f513d3c0c4e533d25058157b2d0",
+    "url": "/react-gallery/static/media/react.fbf20f51.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gallery/precache-manifest.8aa4e9e41c6caeba5728179724aeb029.js"
+  "/react-gallery/precache-manifest.e8206a9618ba0a14943baa2dde0cef57.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gallery/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
